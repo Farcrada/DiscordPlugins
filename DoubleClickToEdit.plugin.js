89aa6aefcd43b5f7f48ee17486ea55e38b90cab7
Farcrada Withman <Farcrada@gmail.com>
2019-07-11 18:52:40 +0200

    Changed the way it finds "Edit"
    Added a search to prevent errors and get it working again.

--- DoubleClickToEdit.plugin.js
+++ DoubleClickToEdit.plugin.js
@@ -1,9 +1,9 @@
-//META{"name":"DoubleClickToEdit","displayName":"DoubleClickToEdit","website":"https://github.com/Farcrada/dblClickEdit.plugin.js","source":"https://github.com/Farcrada/blob/master/dblClickEdit.plugin.js"}*//
+//META{"name":"DoubleClickToEdit","displayName":"DoubleClickToEdit","website":"https://github.com/Farcrada/Double-click-to-edit","source":"https://github.com/Farcrada/Double-click-to-edit/blob/master/DoubleClickToEdit.plugin.js"}*//
 
 class DoubleClickToEdit {
     getName() { return "Double click to edit"; }
     getDescription() { return "Double click messages to edit them."; }
-    getVersion() { return "0.3.0"; }
+    getVersion() { return "0.4.3"; }
     getAuthor() { return "Farcrada, original by Jiiks"; }
 
     start() {
@@ -34,7 +34,7 @@ class DoubleClickToEdit {
         const btn = message.querySelector('[class^=buttonContainer] [class^=button-]');
         if (!btn)
             return;
-
+        
         btn.click();
 
         const popup = document.querySelector('[class^=container][role=menu]');
@@ -42,9 +42,30 @@ class DoubleClickToEdit {
             return;
 
         const rii = popup[Object.keys(popup).find(k => k.startsWith('__reactInternal'))];
-        if (!rii || !rii.memoizedProps || !rii.memoizedProps.children || !rii.memoizedProps.children[1] || !rii.memoizedProps.children[1].props || !rii.memoizedProps.children[1].props.onClick)
-            return;
 
-        rii.memoizedProps.children[1].props.onClick();
+        if (!rii || !rii.memoizedProps || !rii.memoizedProps.children)
+        {
+            btn.click();
+            return;
+        }
+
+        for (var i = 0; i < rii.memoizedProps.children.length; i++)
+        {
+            if (!rii.memoizedProps.children[i])
+                continue;
+            if (!rii.memoizedProps.children[i].props)
+                continue;
+            if (!rii.memoizedProps.children[i].props.children)
+                continue;
+            if (!rii.memoizedProps.children[i].props.children.startsWith)
+                continue;
+            if (!rii.memoizedProps.children[i].props.children.startsWith('Edit'))
+                continue;
+            
+            rii.memoizedProps.children[i].props.onClick();
+            return;
+        }
+
+        btn.click();
     }
 }
