69e52e52a6e5262ce61d581791b6446e040de725
Farcrada Withman <Farcrada@gmail.com>
2020-02-14 21:07:59 +0100

    See: https://github.com/Farcrada/Double-click-to-edit/commit/5f7a0a5c91efab0097cf68dacdbaafce6c68e792
    See: https://github.com/Farcrada/Double-click-to-edit/commit/5f7a0a5c91efab0097cf68dacdbaafce6c68e792
    
    (See original file update)

--- DoubleClickToEditPTB.plugin.js
+++ DoubleClickToEditPTB.plugin.js
@@ -1,9 +1,9 @@
-//META{"name":"DoubleClickToEditPTB","displayName":"DoubleClickToEditPTB","website":"https://github.com/Farcrada/Double-click-to-edit","source":"https://github.com/Farcrada/Double-click-to-edit/blob/master/DoubleClickToEditPTB.plugin.js"}*//
+//META{"name":"DoubleClickToEdit","displayName":"DoubleClickToEdit","website":"https://github.com/Farcrada/Double-click-to-edit","source":"https://github.com/Farcrada/Double-click-to-edit/blob/master/DoubleClickToEdit.plugin.js"}*//
 
-class DoubleClickToEditPTB {
-    getName() { return "Double click to edit PTB"; }
-    getDescription() { return "Double click messages to edit them. (PTB version)"; }
-    getVersion() { return "8.0.0"; }
+class DoubleClickToEdit {
+    getName() { return "Double click to edit"; }
+    getDescription() { return "Double click messages to edit them."; }
+    getVersion() { return "9.0.0"; }
     getAuthor() { return "Farcrada, original by Jiiks"; }
 
     start() {
@@ -18,7 +18,7 @@ class DoubleClickToEditPTB {
         }
 
         if (window.ZLibrary) this.initialize();
-        
+
         try {
             document.addEventListener('dblclick', this.handler);
         }
@@ -35,7 +35,7 @@ class DoubleClickToEditPTB {
     }
 
     initialize() {
-        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/Farcrada/Double-click-to-edit/master/DoubleClickToEditPTB.plugin.js");
+        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/Farcrada/Double-click-to-edit/master/DoubleClickToEdit.plugin.js");
     }
 
     stop() {
@@ -43,29 +43,54 @@ class DoubleClickToEditPTB {
     }
     
     handler(e) {
-        const message = e.target.closest('[class^=message]');
+        let messagediv = e.target.closest('[class^=message]');
+
+        if (!messagediv)
+            return;
+
+        let instance = messagediv[Object.keys(messagediv).find(key => key.startsWith("__reactInternal"))];
+        let message = instance && findValue(instance, "message");
+        
         if (!message)
             return;
+
+        if (message.author.id !== BdApi.findModuleByProps("getCurrentUser").getCurrentUser().id)
+            return;
+
+        BdApi.findModuleByProps("receiveMessage", "editMessage").startEditMessage(message.channel_id, message.id, message.content);
         
-        const btn = message.querySelector('[class^=buttonContainer] [class^=button-][aria-label=More]');
-        if (!btn)
-            return;
-        btn.click();
+        function findValue (instance, searchkey) {
+            var whitelist = {
+                memoizedProps: true,
+                child: true,
+            };
+            var blacklist = {
+                contextSection: true
+            };
+            var singlekey = getKey(instance);
+            return singlekey;
+            
+            function getKey(instance) {
+                var result = undefined;
+                if (instance && !Node.prototype.isPrototypeOf(instance)) {
+                    let keys = Object.getOwnPropertyNames(instance);
+                    for (let i = 0; result === undefined && i < keys.length; i++) {
+                        let key = keys[i];
 
-        const popup = document.querySelector('[class^=contextMenu]');
-        if (!popup)
-            return;
+                        if (key && !blacklist[key]) {
+                            var value = instance[key];
+                            
+                            if (searchkey === key)
+                                result = value;
 
-        const rii = popup[Object.keys(popup).find(k => k.startsWith('__reactInternal'))];
-        if (!rii || !rii.memoizedProps || !rii.memoizedProps.children[0]
-            || !rii.memoizedProps.children[0].props || !rii.memoizedProps.children[0].props.children[0]
-            || !rii.memoizedProps.children[0].props.children[0].props
-            || !rii.memoizedProps.children[0].props.children[0].props.action)
-        {
-            btn.click();
-            return;
+                            else if ((typeof value === "object" || typeof value === "function") &&
+                                    (whitelist[key] || key[0] == "." || !isNaN(key[0])))
+                                result = getKey(value);
+                        }
+                    }
+                }
+                return result;
+            }
         }
-        rii.memoizedProps.children[0].props.children[0].props.action();
-        return;
     }
 }
