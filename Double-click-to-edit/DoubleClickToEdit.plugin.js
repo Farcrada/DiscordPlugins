3d6d07c09c1241e5365c12427d140e2fa9884a91
Farcrada Withman <Farcrada@protonmail.com>
2021-03-15 23:08:53 +0000

    Resolved https://github.com/Farcrada/DiscordPlugins/issues/20
    Resolved by attempting to search for a `baseMessage` of a reply-to message (Which in chronological terms seems rather backwards.).
    
    Also cleaned up the code (less stress on runtime/smoother operation):
     - Moved out the functions
     - Attempted and succeeded to lessen the calls made to store and request values on execution.

--- Double-click-to-edit/DoubleClickToEdit.plugin.js
+++ Double-click-to-edit/DoubleClickToEdit.plugin.js
@@ -1,9 +1,15 @@
-//META{"name":"DoubleClickToEdit","displayName":"DoubleClickToEdit","website":"https://github.com/Farcrada/DiscordPlugins/Double-click-to-edit","source":"https://github.com/Farcrada/DiscordPlugins/blob/master/Double-click-to-edit/DoubleClickToEdit.plugin.js"}*//
+/**
+ * @name DoubleClickToEdit
+ * 
+ * @website https://github.com/Farcrada/DiscordPlugins/
+ * @source https://github.com/Farcrada/DiscordPlugins/blob/master/Double-click-to-edit/DoubleClickToEdit.plugin.js
+ */
+
 
 class DoubleClickToEdit {
     getName() { return "Double click to edit"; }
     getDescription() { return "Double click messages to edit them."; }
-    getVersion() { return "9.0.5"; }
+    getVersion() { return "9.0.6"; }
     getAuthor() { return "Farcrada, original by Jiiks"; }
 
     start() {
@@ -22,13 +28,13 @@ class DoubleClickToEdit {
         try {
             document.addEventListener('dblclick', this.handler);
         }
-        catch(err) {
+        catch (err) {
             console.error(this.getName(), "fatal error, plugin could not be started!", err);
-            
+
             try {
                 this.stop();
             }
-            catch(err) {
+            catch (err) {
                 console.error(this.getName() + ".stop()", err);
             }
         }
@@ -36,12 +42,14 @@ class DoubleClickToEdit {
 
     initialize() {
         ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/Farcrada/DiscordPlugins/master/Double-click-to-edit/DoubleClickToEdit.plugin.js");
+
+        DoubleClickToEdit.myID = BdApi.findModuleByProps("getCurrentUser").getCurrentUser().id;
     }
 
     stop() {
         document.removeEventListener('dblclick', this.handler);
     }
-    
+
     handler(e) {
         let messagediv = e.target.closest('[class^=message]');
 
@@ -49,49 +57,56 @@ class DoubleClickToEdit {
             return;
 
         let instance = messagediv[Object.keys(messagediv).find(key => key.startsWith("__reactInternal"))];
-        let message = instance && findValue(instance, "message");
-        
-        if (!message)
-            return;
+        let message = instance && getValueFromKey(instance, "message");
 
-        if (message.author.id !== BdApi.findModuleByProps("getCurrentUser").getCurrentUser().id)
-            return;
+        if (!messageExists(message, DoubleClickToEdit.myID)) {
+            message = instance && getValueFromKey(instance, "baseMessage");
+            if (!messageExists(message, DoubleClickToEdit.myID))
+                return;
+        }
 
+        //Execution
         BdApi.findModuleByProps("receiveMessage", "editMessage").startEditMessage(message.channel_id, message.id, message.content);
-        
-        function findValue (instance, searchkey) {
-            var whitelist = {
-                memoizedProps: true,
-                child: true,
-                sibling: true
-            };
-            var blacklist = {
-                contextSection: true
-            };
-            
-            return getKey(instance);
-            
-            function getKey(instance) {
-                var result = undefined;
-                if (instance && !Node.prototype.isPrototypeOf(instance)) {
-                    let keys = Object.getOwnPropertyNames(instance);
-                    for (let i = 0; result === undefined && i < keys.length; i++) {
-                        let key = keys[i];
+    }
+}
 
-                        if (key && !blacklist[key]) {
-                            var value = instance[key];
-                            
-                            if (searchkey === key)
-                                result = value;
+var whitelist = {
+    memoizedProps: true,
+    child: true,
+    sibling: true
+};
+var blacklist = {
+    contextSection: true
+};
 
-                            else if ((typeof value === "object" || typeof value === "function") &&
-                                    (whitelist[key] || key[0] == "." || !isNaN(key[0])))
-                                result = getKey(value);
-                        }
-                    }
-                }
-                return result;
+function messageExists(message, id) {
+    if (!message)
+        return false;
+
+    if (message.author.id !== id)
+        return false;
+
+    return true;
+}
+
+function getValueFromKey(instance, searchkey) {
+    var result = undefined;
+    if (instance && !Node.prototype.isPrototypeOf(instance)) {
+        let keys = Object.getOwnPropertyNames(instance);
+        for (let i = 0; result === undefined && i < keys.length; i++) {
+            let key = keys[i];
+
+            if (key && !blacklist[key]) {
+                var value = instance[key];
+
+                if (searchkey === key)
+                    result = value;
+
+                else if ((typeof value === "object" || typeof value === "function") &&
+                    (whitelist[key] || key[0] == "." || !isNaN(key[0])))
+                    result = getValueFromKey(value, searchkey);
             }
         }
     }
+    return result;
 }
