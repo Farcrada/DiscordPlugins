ac1bdbcbb72afee5c79e229d475ded78a4c924b0
Farcrada Withman <Farcrada@protonmail.com>
2021-07-16 14:37:48 +0000

    Code optimizations

--- Right-Click-Join/RightClickJoin.plugin.js
+++ Right-Click-Join/RightClickJoin.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name RightClickJoin
  * @author Farcrada
- * @version 1.1.7
+ * @version 1.2.0
  * @description Right click a user to join a voice channel they are in.
  * 
  * @website https://github.com/Farcrada/DiscordPlugins
@@ -15,7 +15,7 @@ const config = {
         name: "Right Click Join",
         id: "RightClickJoin",
         description: "Right click a user to join a voice channel they are in.",
-        version: "1.1.7",
+        version: "1.2.0",
         author: "Farcrada",
         updateUrl: "https://raw.githubusercontent.com/Farcrada/DiscordPlugins/master/Right-Click-Join/RightClickJoin.plugin.js"
     }
@@ -58,7 +58,7 @@ class RightClickJoin {
         }
         catch (err) {
             try {
-                console.error("Attempting to stop after initialization error...")
+                console.error("Attempting to stop after initialization error...", err)
                 this.stop();
             }
             catch (err) {
@@ -69,127 +69,130 @@ class RightClickJoin {
 
     initialize() {
         //Create our cache
-        createCache();
+        this.createCache();
 
         //Patch the guild context menu
-        patchGuildChannelUserContextMenu();
+        this.patchGuildChannelUserContextMenu();
         //And since it would be handy to join from a DM, it works differently.
-        patchDMUserContextMenu();
+        this.patchDMUserContextMenu();
+    }
+
+    createCache() {
+        //The modules for context menus related to users we want to patch.
+        this.guildUserContextMenus = BdApi.findModule(m => m.default && m.default.displayName === "GuildChannelUserContextMenu");
+        this.dmUserContextMenu = BdApi.findModule(m => m.default && m.default.displayName === "DMUserContextMenu");
+    
+        //Specific functions we need, nothing like a big module
+        this.getVoiceStatesForChannel = BdApi.findModuleByProps("getAllVoiceStates", "getVoiceStatesForChannel").getVoiceStatesForChannel;
+        this.getChannels = BdApi.findModuleByProps("getChannels", "getDefaultChannel").getChannels;
+        this.selectVoiceChannel = BdApi.findModuleByProps("selectChannel").selectVoiceChannel;
+        this.fetchProfile = BdApi.findModuleByProps("fetchProfile").fetchProfile;
+    
+        //GuildStore
+        this.GuildStore = BdApi.findModuleByProps("getGuild", "getGuilds");
+        this.ChannelStore = BdApi.findModuleByProps("getChannel", "getDMFromUserId");
+        this.MutualStore = BdApi.findModuleByProps("isFetching", "getUserProfile");
+    
+        //React and shit
+        this.ce = BdApi.React.createElement;
+        //Context controls (mainly just the one item we insert)
+        this.MenuItem = BdApi.findModuleByProps("MenuRadioItem", "MenuItem").MenuItem;
     }
 
     stop() { BdApi.Patcher.unpatchAll(config.info.id); }
-}
 
-function createCache() {
-    //What context menus we want to patch
-    //Allllll the context menus related to users Let's see what sticks.
-    RightClickJoin.guildUserContextMenus = BdApi.findModule(m => m.default && m.default.displayName === "GuildChannelUserContextMenu");
-    RightClickJoin.dmUserContextMenu = BdApi.findModule(m => m.default && m.default.displayName === "DMUserContextMenu");
+    
+    patchGuildChannelUserContextMenu() {
+        //Patch in our context item under our name
+        BdApi.Patcher.after(config.info.id, this.guildUserContextMenus, "default", (that, [props], returnValue) => {
+            //Enter the world of patching
+    
+            let indexObject = { section: 1, child: 3 };
+            let channel = this.ChannelStore.getChannel(props.channelId)
+    
+            if (channel.isVocal())
+                //if we right click a channel in the list, we can mitigate our intense searching.
+                this.constructMenuItem(props.user.id, returnValue, indexObject, channel.id)
+            else
+                //Drop right into it for the guilds; Don't have to catch the return either.
+                this.checkMenuItem(this.getChannels(props.guildId).VOCAL, props.user.id, returnValue, indexObject)
+    
+        });
+    }
+    
+    patchDMUserContextMenu() {
+        //Patch in our context item under our name
+        BdApi.Patcher.after(config.info.id, this.dmUserContextMenu, "default", (that, [props], returnValue) => {
+            //Enter the world of patching
+    
+            //Now we gotta check mutual guilds to see if we match anything
+            let userId = props.user.id;
+            //                                     Dumb shit, null checking 'n all.
+            let mutualGuilds = this.MutualStore.getMutualGuilds(userId) ?? [];
+            let indexObject = { section: 2, child: 1 };
 
-    //Specific functions we need, nothing like a big module
-    RightClickJoin.getVoiceStatesForChannel = BdApi.findModuleByProps("getAllVoiceStates", "getVoiceStatesForChannel").getVoiceStatesForChannel;
-    RightClickJoin.getChannels = BdApi.findModuleByProps("getChannels", "getDefaultChannel").getChannels;
-    RightClickJoin.selectVoiceChannel = BdApi.findModuleByProps("selectChannel").selectVoiceChannel;
-    RightClickJoin.fetchProfile = BdApi.findModuleByProps("fetchProfile").fetchProfile;
+            //The `fetchProfile()` promise fucks the this binding and `.bind(this)` doesn't work, but this does :)
+            let self = this;
 
-    //GuildStore
-    RightClickJoin.GuildStore = BdApi.findModuleByProps("getGuild", "getGuilds");
-    RightClickJoin.ChannelStore = BdApi.findModuleByProps("getChannel", "getDMFromUserId");
-    RightClickJoin.MutualStore = BdApi.findModuleByProps("isFetching", "getUserProfile");
-
-    //React and shit
-    RightClickJoin.ce = BdApi.React.createElement;
-    //Context controls (mainly just the one item we insert)
-    RightClickJoin.MenuItem = BdApi.findModuleByProps("MenuRadioItem", "MenuItem").MenuItem;
-}
-
-function patchGuildChannelUserContextMenu() {
-    //Patch in our context item under our name
-    BdApi.Patcher.after(config.info.id, RightClickJoin.guildUserContextMenus, "default", (that, [props], returnValue) => {
-        //Enter the world of patching
-
-        let indexObject = { section: 1, child: 3 };
-        let channel = RightClickJoin.ChannelStore.getChannel(props.channelId)
-
-        if (channel.isVocal())
-            //if we right click a channel in the list, we can mitigate our intense searching.
-            constructMenuItem(props.user.id, returnValue, indexObject, channel.id)
-        else
-            //Drop right into it for the guilds; Don't have to catch the return either.
-            checkMenuItem(RightClickJoin.getChannels(props.guildId).VOCAL, props.user.id, returnValue, indexObject)
-
-    });
-}
-
-function patchDMUserContextMenu() {
-    //Patch in our context item under our name
-    BdApi.Patcher.after(config.info.id, RightClickJoin.dmUserContextMenu, "default", (that, [props], returnValue) => {
-        //Enter the world of patching
-
-        //Now we gotta check mutual guilds to see if we match anything
-        let userId = props.user.id;
-        //                                                    Dumb shit, null checking 'n all.
-        let mutualGuilds = RightClickJoin.MutualStore.getMutualGuilds(userId) ?? [];
-        let indexObject = { section: 2, child: 1 };
-
-        if (mutualGuilds.length < 1) {
-            //Gotta make sure we're not fetching already (or risk spamming the API)
-            if (RightClickJoin.MutualStore.isFetching(userId))
-                return;
-
-            //Fetch and then we need to fill "mutualGuilds" again, so we just pass the call
-            RightClickJoin.fetchProfile(userId).then(_ => dmPatchHandler(RightClickJoin.MutualStore.getMutualGuilds(userId)));
-        }
-        else
-            dmPatchHandler(mutualGuilds)
-
-        function dmPatchHandler(_mutualGuilds = []) {
-            //So we need a loop through if there's many
-            for (let i = 0; i < _mutualGuilds.length; i++)
-                //We need to have a way to break early
-                //if we find anything to reduce resource consumption
-                //You can only be connected to one voicechannel anyway
-                if (checkMenuItem(RightClickJoin.getChannels(_mutualGuilds[i].guild.id).VOCAL, userId, returnValue, indexObject))
-                    break;
-        }
-    });
-}
-
-function checkMenuItem(voiceChannels, userId, returnValue, indexObject) {
-    //Gotta make sure this man is actually in a voice call
-    //Loopy whoop
-    for (let i = 0; i < voiceChannels.length; i++)
-        if (constructMenuItem(userId, returnValue, indexObject, voiceChannels[i].channel.id))
-            return true;
-    //Return false so our DM patch knows what to do.
-    return false;
-}
-
-function constructMenuItem(userId, returnValue, indexObject, channelId) {
-    //Get all the participants in this voicechannel
-    let participants = RightClickJoin.getVoiceStatesForChannel(channelId);
-
-    //Loopy doop
-    for (let id in participants)
-        //If a matching participant is found, engage
-        if (participants[id].userId === userId) {
-            //Splice and insert our context item
-            //          the menu,      the sections,     the items of this section
-            returnValue.props.children.props.children[indexObject.section].props.children.splice(
-                //We want it after the "call" option.
-                indexObject.child,
-                0,
-                RightClickJoin.ce(RightClickJoin.MenuItem, {
-                    //Discord Is One Of Those
-                    label: "Join Call",
-                    id: config.info.name.toLowerCase().replace(' ', '-'),
-                    action: () => {
-                        //Joining a voicechannel
-                        RightClickJoin.selectVoiceChannel(channelId);
-                    }
-                })
-            );
-            //Return entirely, since only one voicechannel is possible.
-            return true;
-        }
+            if (mutualGuilds.length < 1) {
+                //Gotta make sure we're not fetching already (or risk spamming the API)
+                if (this.MutualStore.isFetching(userId))
+                    return;
+    
+                //Fetch and then we need to fill "mutualGuilds" again, so we just pass the call
+                this.fetchProfile(userId).then(_ => dmPatchHandler(this.MutualStore.getMutualGuilds(userId)));
+            }
+            else
+                dmPatchHandler(mutualGuilds)
+    
+            function dmPatchHandler(_mutualGuilds = []) {
+                //So we need a loop through if there's many
+                for (let i = 0; i < _mutualGuilds.length; i++)
+                    //We need to have a way to break early if we find anything,
+                    //to reduce resource consumption.
+                    //You can only be connected to one voicechannel anyway
+                    if (self.checkMenuItem(self.getChannels(_mutualGuilds[i].guild.id).VOCAL, userId, returnValue, indexObject))
+                        break;
+            }
+        });
+    }
+    
+    checkMenuItem(voiceChannels, userId, returnValue, indexObject) {
+        //Gotta make sure this man is actually in a voice call
+        //Loopy whoop
+        for (let i = 0; i < voiceChannels.length; i++)
+            if (this.constructMenuItem(userId, returnValue, indexObject, voiceChannels[i].channel.id))
+                return true;
+        //Return false so our DM patch knows what to do.
+        return false;
+    }
+    
+    constructMenuItem(userId, returnValue, indexObject, channelId) {
+        //Get all the participants in this voicechannel
+        let participants = this.getVoiceStatesForChannel(channelId);
+    
+        //Loopy doop
+        for (let id in participants)
+            //If a matching participant is found, engage
+            if (participants[id].userId === userId) {
+                //Splice and insert our context item
+                //                the menu,    the sections,               the items of this section
+                returnValue.props.children.props.children[indexObject.section].props.children.splice(
+                    //We want it after the "call" option.
+                    indexObject.child,
+                    0,
+                    this.ce(this.MenuItem, {
+                        //Discord Is One Of Those
+                        label: "Join Call",
+                        id: config.info.name.toLowerCase().replace(' ', '-'),
+                        action: () => {
+                            //Joining a voicechannel
+                            this.selectVoiceChannel(channelId);
+                        }
+                    })
+                );
+                //Return entirely, since only one voicechannel is possible.
+                return true;
+            }
+    }
 }
