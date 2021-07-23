ca490173fc8ab0f11647f0bfa585a96a172ad7b2
Farcrada Withman <Farcrada@protonmail.com>
2021-07-23 22:55:16 +0000

    Added keybind support resolve: https://github.com/Farcrada/DiscordPlugins/issues/43

--- Hide-Channels/HideChannels.plugin.js
+++ Hide-Channels/HideChannels.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name HideChannels
  * @author Farcrada
- * @version 1.1.0
+ * @version 1.2.0
  * @description Hide channel list from view.
  * 
  * @website https://github.com/Farcrada/DiscordPlugins
@@ -15,7 +15,7 @@ const config = {
         name: "Hide Channels",
         id: "HideChannels",
         description: "Hide channel list from view.",
-        version: "1.1.0",
+        version: "1.2.0",
         author: "Farcrada",
         updateUrl: "https://raw.githubusercontent.com/Farcrada/DiscordPlugins/master/Hide-Channels/HideChannels.plugin.js"
     },
@@ -84,6 +84,38 @@ class HideChannels {
         }
     }
 
+    getSettingsPanel() {
+        return BdApi.React.createElement(this.FormItem, {
+            title: "Toggle by keybind:"
+        },
+            BdApi.React.createElement(this.KeybindRecorder, {
+                defaultValue: this.keybindSetting,
+                onChange: keybind => this.saveKeybind(keybind)
+            }));
+    }
+
+    //Saving
+    saveKeybind(keybind) {
+        this.keybind = this.setKeybind(keybind);
+        BdApi.saveData(config.info.id, "keybind", keybind);
+    }
+
+    //Keydown event
+    keydownFunc = (e) => {
+        //Since we made this an object,
+        //we can make new propertire with `[]`
+        this.currentlyPressed[e.keyCode] = true;
+    }
+    //Keyup event
+    keyupFunc = (e) => {
+        //Check if every currentlyPessed is in our saved keybind.
+        if (this.keybind.every(key => this.currentlyPressed[key] === true))
+            this.toggleChannels();
+
+        //Reset currently pressed
+        this.currentlyPressed = {};
+    }
+
     //Everytime we switch the chat window is reloaded;
     //as a result we need to check and potentially render the button again.
     onSwitch() {
@@ -99,6 +131,15 @@ class HideChannels {
 
         //Need to make sure we can track the position.
         this.channelsHiddenBool = false;
+        //And the keybind
+        this.keybindSetting = this.checkKeybindLoad(BdApi.loadData(config.info.id, "keybind"));
+        this.keybind = this.setKeybind(this.keybindSetting);
+        //Predefine current keybind
+        this.currentlyPressed = {};
+
+        //React functions for settings
+        this.FormItem = BdApi.findModuleByProps("FormItem").FormItem;
+        this.KeybindRecorder = BdApi.findModuleByDisplayName("KeybindRecorder");
 
         //Check if there is any CSS we have already, and remove it.
         BdApi.clearCSS(config.constants.cssStyle);
@@ -139,8 +180,36 @@ class HideChannels {
         .${this.sidebarClass} {
             transition: width 400ms ease;
         }`);
+
+        //Make sure we listen for a keydown action
+        document.addEventListener("keydown", this.keydownFunc, true);
+        //And execute on a keyup action
+        document.addEventListener("keyup", this.keyupFunc, true);
+
         //Render the button and we're off to the races!
         this.renderButton();
+    }
+
+    //Remove and cleanup
+    stop() {
+        //Our CSS
+        BdApi.clearCSS(config.constants.cssStyle);
+
+        //Our button
+        let button = document.getElementById(config.constants.buttonID);
+        if (button)
+            button.remove();
+
+        //And if there are remnants of css left,
+        //make sure we remove the class from the sidebar to ensure visual confirmation.
+        let sidebar = document.querySelector(`.${this.sidebarClass}`);
+        if (sidebar.classList.contains(config.constants.hideElementsName))
+            sidebar.classList.remove(config.constants.hideElementsName);
+
+
+        document.removeEventListener("keydown", this.keydownFunc, true);
+        //And execute on a keyup action
+        document.removeEventListener("keyup", this.keyupFunc, true);
     }
 
     //Creation and appending our button, i.e. rendering.
@@ -152,7 +221,7 @@ class HideChannels {
         //If there is no title bar, dump
         if (!titleBar)
             return;
-        
+
         //Set ID for easy targeting.
         button.setAttribute('id', config.constants.buttonID);
         //Set class according to the current visibility
@@ -189,20 +258,56 @@ class HideChannels {
         }
     }
 
-    //Remove and cleanup
-    stop() {
-        //Our CSS
-        BdApi.clearCSS(config.constants.cssStyle);
+    //These could be statics, but looks scuffed.
+    //Nullchecking
+    checkKeybindLoad(keybindSetting) {
+        for (const keybind of keybindSetting) {
+            if (Array.isArray(keybind)) {
+                for (const key of keybind)
+                    if (typeof (key) !== "number")
+                        return [[0, 162], [0, 72]];
+            }
+            else if (typeof (keybind) !== "number")
+                return [[0, 162], [0, 72]];
 
-        //Our button
-        let button = document.getElementById(config.constants.buttonID);
-        if (button)
-            button.remove();
+        }
+        return keybindSetting;
+    }
 
-        //And if there are remnants of css left,
-        //make sure we remove the class from the sidebar to ensure visual confirmation.
-        let sidebar = document.querySelector(`.${this.sidebarClass}`);
-        if (sidebar.classList.contains(config.constants.hideElementsName))
-            sidebar.classList.remove(config.constants.hideElementsName);
+    //Filter that shit
+    setKeybind(keybind) {
+        //We need to filter the tab, ctrl, alt and shift keys because it's scuffed.
+        //Lets fix it.
+        return keybind.map(keyCode => {
+            if (Array.isArray(keyCode[0]))
+                for (let i = 0; i < keyCode.length; i++)
+                    keyCode[i] = fixCode(keyCode[i])
+            else
+                keyCode = fixCode(keyCode);
+
+            return keyCode;
+
+            function fixCode(code) {
+                //code[0] is always 0, lets make this simpel for us.
+                code = code[1];
+                switch (code) {
+                    case 20:                    //Tab: 20 -> 9
+                        return 9;
+                    //Fallthrough since it's the same
+                    case 160:                   //Shift: 160 -> 16 
+                    case 161:                   //R Shift: 161 -> 16
+                        return 16;
+                    //Again
+                    case 162:                   //Control: 162 -> 17
+                    case 163:                   //R Control: 163 -> 17
+                        return 17;
+                    //And again.
+                    case 164:                   //Alt: 164 -> 18
+                    case 165:                   //R Alt: 165 ->  18
+                        return 18;
+                    default: return code;       //Other keys? return;
+                }
+            }
+        });
     }
 }
